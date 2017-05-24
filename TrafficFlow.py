4209f965ae49446c55da59b466b9fd3de96c9564
fjbriones <fjbriones@outlook.com>
2017-05-24 13:28:14 +0800

    Finalize code

--- TrafficFlow.py
+++ TrafficFlow.py
@@ -7,6 +7,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 import pandas
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def read_dataset(filename, columns, scaler):
     dataframe = pandas.read_csv(filename, usecols=columns, engine='python')
@@ -42,6 +43,14 @@ def load_model(filename):
     print('Model is loaded')
     return loaded_model
 
+def hard_tanh (x):
+    return tf.minimum(tf.maximum(x, -1.), 1.)
+
+def noisy_input(x, c=0.05):
+    noise = np.random.normal(loc=0.0, scale=1.0, size=np.shape(x))
+    output = x + c*noise
+    return output
+
 def model_lstm_train(units, trainX, trainY, epochs, batch_size, validation_split):
     model = Sequential()
     model.add(LSTM(units, input_shape=(time_step, 1)))
@@ -64,6 +73,52 @@ def model_gru_train(units, trainX, trainY, epochs, batch_size, validation_split)
 
     return model
 
+def model_nan_lstm_train(units, trainX, trainY, epochs, batch_size, validation_split):
+    model = Sequential()
+    model.add(LSTM(units, input_shape=(time_step, 1), activation=hard_tanh))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2, validation_split=validation_split)
+
+    save_model('models/NAN_LSTM_model', model)
+
+    return model
+
+def model_nan_gru_train(units, trainX, trainY, epochs, batch_size, validation_split):
+    model = Sequential()
+    model.add(GRU(units, input_shape=(time_step, 1), activation=hard_tanh))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2, validation_split=validation_split)
+
+    save_model('models/NAN_GRU_model',model)
+
+    return model
+
+def model_nani_lstm_train(units, trainX, trainY, epochs, batch_size, validation_split):
+    trainX = noisy_input(trainX)
+    model = Sequential()
+    model.add(LSTM(units, input_shape=(time_step, 1), activation=hard_tanh))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2, validation_split=validation_split)
+
+    save_model('models/NANI_LSTM_model', model)
+
+    return model
+
+def model_nani_gru_train(units, trainX, trainY, epochs, batch_size, validation_split):
+    trainX=noisy_input(trainX)
+    model = Sequential()
+    model.add(GRU(units, input_shape=(time_step, 1), activation=hard_tanh))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2, validation_split=validation_split)
+
+    save_model('models/NANI_GRU_model',model)
+
+    return model
+
 def flow_predict(x_data, x_true, model, model_type):
     dataPredict = model.predict(x_data)
 
@@ -82,8 +137,8 @@ def flow_plot(x_pred, x_true, len):
     truePlot = truePlot[0:len]
     predPlot = x_pred[0:len]
 
-    trueP = plt.plot(truePlot, label='True Plot')
-    predP = plt.plot(predPlot, label='Predicted Plot')
+    plt.plot(truePlot, label='True Plot')
+    plt.plot(predPlot, label='Predicted Plot')
     plt.legend()
     plt.show()
 
@@ -96,19 +151,37 @@ test = read_dataset('data/test_dataset.csv', [1], scaler)
 
 time_step = 6
 len_day = 288 #len_day in terms of 5 minutes
+epochs = 50
+batch_size = 32
+units = 32
+validation_split = 0.2
+
+#Create datasets
 trainX, trainY = create_dataset(train, time_step)
 testX, testY = create_dataset(test, time_step)
-
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
 testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 
-#modelLSTM = model_lstm_train(64, trainX, trainY, epochs=2000, batch_size=64, validation_split=0.2)
+modelLstm = model_lstm_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+modelGru = model_gru_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
 
-#modelGRU = model_gru_train(64, trainX, trainY, epochs=2000, batch_size=64, validation_split=0.2)
+modelNanLstm= model_nan_lstm_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+modelNanGru = model_nan_gru_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
 
-modelLSTM = load_model('models/LSTM_model')
+modelNaniLstm= model_nani_lstm_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
+modelNaniGru = model_nani_gru_train(units, trainX, trainY, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
 
-predictedTrainLSTM, trainY = flow_predict(trainX, trainY, modelLSTM, 'LSTM ')
-predictedTestLSTM, testY = flow_predict(testX, testY, modelLSTM, 'LSTM ')
+predictedTrainLstm, trainLstm = flow_predict(trainX, trainY, modelLstm, 'LSTM Training ')
+predictedTestLstm, testLstm = flow_predict(testX, testY, modelLstm, 'LSTM Testing ')
+predictedTrainGru, trainGru = flow_predict(trainX, trainY, modelGru, 'GRU Training ')
+predictedTestGru, testGru = flow_predict(testX, testY, modelGru, 'GRU Testing ')
 
-flow_plot(predictedTestLSTM, testY, len=len_day)
+predictedTrainNanLstm, trainNanLstm = flow_predict(trainX, trainY, modelNanLstm, 'NAN LSTM Training ')
+predictedTestNanLstm, testNanLstm= flow_predict(testX, testY, modelNanLstm, 'NAN LSTM Testing ')
+predictedTrainNanGru, trainNanGru = flow_predict(trainX, trainY, modelNanGru, 'NAN GRU Training ')
+predictedTestNanGru, testNanGru = flow_predict(testX, testY, modelNanGru, 'NAN GRU Testing ')
+
+predictedTrainNaniLstm, trainNaniLstm = flow_predict(trainX, trainY, modelNaniLstm, 'NANI LSTM Training ')
+predictedTestNaniLstm, testNaniLstm= flow_predict(testX, testY, modelNaniLstm, 'NANI LSTM Testing ')
+predictedTrainNaniGru, trainNaniGru = flow_predict(trainX, trainY, modelNaniGru, 'NANI GRU Training ')
+predictedTestNaniGru, testNaniGru = flow_predict(testX, testY, modelNaniGru, 'NANI GRU Testing ')
